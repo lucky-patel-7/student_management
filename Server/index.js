c018a907e55e8320989c64ce6f4ec030c179c361
lucky-patel-7 <125451527+lucky-patel-7@users.noreply.github.com>
2023-04-04 15:22:33 +0530

    swagger deleted

--- Server/index.js
+++ Server/index.js
@@ -5,29 +5,6 @@ import notFound from './middleware/notFound.js'
 import errorHandler from './middleware/error.js'
 import students from './routes/student.js'
 import teachers from './routes/teacher.js'
-import swaggerUI from 'swagger-ui-express'
-import swaggerJsDoc from 'swagger-jsdoc'
-
-const options = {
-    definition: {
-        openapi: "3.0.0",
-        info: {
-            title: "Student Attendance API",
-            version: "1.0.0",
-            description: "A simple Express Library API",
-        },
-        servers: [
-            {
-                url: "http://127.0.0.1:8000",
-            },
-        ],
-    },
-    apis: ["./Swagger/*.js"],
-};
-
-const specs = swaggerJsDoc(options)
-
-
 
 
 
